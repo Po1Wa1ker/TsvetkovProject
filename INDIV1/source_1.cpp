1f37ef616dec2ff3c8f720955bea451b0c2c2131
Po1Wa1ker <105970284+Po1Wa1ker@users.noreply.github.com>
2022-05-26 19:21:18 +0300

    Add files via upload

---
+++ INDIV1/source_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "company.h"
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");	
+	Company A, B(4, 0), C(4, 3);
+	cout << " Нулевые значения = " << B << endl;
+	cout << " Значения равные 3 = " << C << endl;
+	B.newname("Компания 1");
+	B.namecheck();
+	C.newproduct("Продукт 1");
+	C.productcheck();
+
+	C.checkresults();
+	B.checkstats();
+
+	system("pause");
+	return 0;
+}
